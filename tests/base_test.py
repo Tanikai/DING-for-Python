239ea4e98468aa59c79d4889ebe516659bee1945
Kai Anter <kai.anter@web.de>
2021-01-30 17:24:54 +0100

    Edit: moved test files to own directory

--- tests/base_test.py
+++ tests/base_test.py
@@ -1,6 +1,7 @@
-from ding import dingapi
+import unittest
+from context import dingapi
 
-api = dingapi()
+api = dingapi.ding_requests()
 
 with open('stop_finder.json', 'w') as f:
     f.write(api.stop_finder('heidenheim bahnhof'))
